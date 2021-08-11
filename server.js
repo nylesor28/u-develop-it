e21ceb77367bb745c92c645d5217c74de24ccc65
RD <81473475+nylesor28@users.noreply.github.com>
2021-08-11 12:51:10 -0400

    add db connection to sever file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001
 const app = express();
@@ -7,13 +8,22 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get('/', (req, res)=> {
-    res.json({
-        message: 'Hello World'
-    });
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: '',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election databse.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
-
+//default response for any other request (not found)
 app.use((req,res)=>{
     res.status(404).end();
 })
